aa309f87b7e722da3014965a86931ca5b0d2f16d
Vicente Guerra <vicenteguerrahdez@gmail.com>
2016-01-03 14:00:14 -0600

    fw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,50 +4,120 @@
         <title>Everwear</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/main.css" />
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                background-color: #006390;
-                color: #FFF;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            img{
-                width: 400px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <img src="img/everwear-logo.png" alt="logo">
-                <h2>Everywhere, Everwear</h2>
-                <h3>Everwear Services</h3>
+    <body class="landing">
+
+        <!-- Page Wrapper -->
+            <div id="page-wrapper">
+
+                <!-- Header -->
+                    <header id="header" class="alt">
+                        <h1><a href="index.html">Everwear</a></h1>
+                        <nav id="nav">
+                            <ul>
+                                <li class="special">
+                                    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                                    <div id="menu">
+                                        <ul>
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="generic.html">Generic</a></li>
+                                            <li><a href="elements.html">Elements</a></li>
+                                            <li><a href="#">Sign Up</a></li>
+                                            <li><a href="#">Log In</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+
+                <!-- Banner -->
+                    <section id="banner">
+                        <div class="inner">
+                            <img class="logo" src="images/everwear-logo.png" alt="">
+                            <p>Turning a simple and old jacket <br />
+                            into a smart and new one.</p>
+                            <ul class="actions">
+                            <li><a href="#" class="button special">Purchase now!</a></li>
+                            </ul>
+                        </div>
+                        <a href="#one" class="more scrolly">Learn More</a>
+                    </section>
+
+                <!-- One -->
+                    <section id="one" class="wrapper style1 special">
+                        <div class="inner">
+                            <header class="major">
+                                <h2>Just imagine</h2>
+                                <p>What wouldn't you do if you could interact with technology using your hands while doing the things you love?</p>
+                            </header>
+                            <ul class="icons major">
+                                <li><span class="icon fa-diamond major style1"><span class="label">Lorem</span></span></li>
+                                <li><span class="icon fa-heart-o major style2"><span class="label">Ipsum</span></span></li>
+                                <li><span class="icon fa-code major style3"><span class="label">Dolor</span></span></li>
+                            </ul>
+                        </div>
+                    </section>
+
+                <!-- Two -->
+                    <section id="two" class="wrapper alt style2">
+
+                        <section class="spotlight">
+                            <div class="image"><img src="images/pic01.jpg" alt="" /></div><div class="content">
+                                <h2>Feel comfortable</h2>
+                                <p>Everwear fits in almost every jacket you want to wear, so let's welcome spring, summer, fall, winter and a lot of fun!</p>
+                            </div>
+                        </section>
+                        <section class="spotlight">
+                            <div class="image"><img src="images/pic02.jpg" alt="" /></div><div class="content">
+                                <h2>Fitness</h2>
+                                <p>Everwear can give useful data for you to take care of yourself. So, if you like running or walking you can check out how many calories you've burned.</p>
+                            </div>
+                        </section>
+                        <section class="spotlight">
+                            <div class="image"><img src="images/pic03.jpg" alt="" /></div><div class="content">
+                                <h2>It is always about security</h2>
+                                <p>All electronic components in everwear are isolated from humidity<br />
+                                    and sweat. You won't be worried about dying because an electric shock, you will worry just to have fun.</p>
+                            </div>
+                        </section>
+                    </section>
+
+                <!-- Three -->
+                    <section id="three" class="wrapper style3 special">
+                        <div class="inner">
+                            <header class="major">
+                                <h2>Ready to connect it with the world</h2>
+                                <p>A very important feature Everwear has is the connection to the Internet. Some functionalities just as the calculation of the route you've walked or sending an emergency message will use this connection.  </p>
+                            </header>
+                        </div>
+                    </section>
+                <!-- Footer -->
+                    <footer id="footer">
+                        <ul class="icons">
+                            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                            <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+                            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+                        </ul>
+                        <ul class="copyright">
+                            <li> Everware</li> <a>AT&T 2016 DEVELOPER SUMMIT</a>
+                        </ul>
+                    </footer>
+
             </div>
-        </div>
+
+        <!-- Scripts -->
+            <script src="js/jquery.min.js"></script>
+            <script src="js/jquery.scrollex.min.js"></script>
+            <script src="js/jquery.scrolly.min.js"></script>
+            <script src="js/skel.min.js"></script>
+            <script src="js/util.js"></script>
+            <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+            <script src="``js/main.js"></script>
+
     </body>
 </html>
