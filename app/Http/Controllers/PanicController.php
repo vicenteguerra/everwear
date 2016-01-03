7e12e8cb8624c6c178b175058716df0aa4a2bdf1
Vicente Guerra <vicenteguerrahdez@gmail.com>
2016-01-03 06:44:23 -0600

    Log

--- app/Http/Controllers/PanicController.php
+++ app/Http/Controllers/PanicController.php
@@ -17,6 +17,7 @@ class PanicController extends Controller
     public function index(Request $request)
     {
         echo "GET";
+        error_log("GET " . $request , 3, "../storage/logs/myerror.log");
         var_dump($request);
     }
 
@@ -32,6 +33,7 @@ class PanicController extends Controller
         $name = $request->get('name');
         var_dump($name);
         var_dump($request);
+        error_log("POST " . $request , 3, "../storage/logs/myerror.log");
     }
 
 
