c74d9d2c833745a99aea9f57d3d43f75ebbb6d56
Vicente Guerra <vicenteguerrahdez@gmail.com>
2016-01-03 12:42:14 -0600

    Update Location, Event and Notification

--- app/Http/Controllers/PanicController.php
+++ app/Http/Controllers/PanicController.php
@@ -36,21 +36,15 @@ class PanicController extends Controller
     public function sendPanicAlert(Request $request)
     {
         echo "POST PANIC ALERT !";
-        //$trigger_name = $request->get('trigger');
-        //$location = $request->get('location'); // latitude, longitude, name, elevation, timestamp
-        //$device = $request->get('device'); // id, name, serial
-        //$values = $request->get('values'); // custom_data
-        //var_dump($device);
-        //var_dump($request);
 
         $location['latitude'] = $request->get('latitude');
         $location['longitude'] = $request->get('longitude');
         $location['elevation'] = $request->get('elevation');
         $message = $request->get('message');
 
-       // $this->updateEventM2X('2');
-        $this->sendLocationM2X($location);
-        $this->sendTrigger($location, $message);
+         $this->updateEventM2X('1');
+         $this->sendLocationM2X($location);
+         $this->sendTrigger($location, $message);
 
         error_log("POST " . $request , 3, "../storage/logs/myerror.log");
         //$this->sendTrigger();
@@ -75,40 +69,39 @@ class PanicController extends Controller
     }
 
     public function updateEventM2X($alert_code){
-        $url = 'http://api-m2x.att.com/v2/devices/' . SELF::DEVICE_ID . '/streams/' . SELF::STREAM . '/value';
-        $data = http_build_query(array('value' => $alert_code));
 
-        $options = array(
-            'http' => array(
-                'header'  => "X-M2X-KEY: ". SELF::API_KEY . ",
-                              Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'PUT',
-                'content' => $data
-            )
-        );
+        $url = "http://api-m2x.att.com/v2/devices/" . SELF::DEVICE_ID . "/streams/" . SELF::STREAM . "/value";
+        $data = array('value' => $alert_code);
+        $json = json_encode($data);
 
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        error_log("Update Event M2X Succesful " . $result , 3, "../storage/logs/myerror.log");
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); // -X
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-M2X-KEY: ace59ecc3eec57c843e0afcc1eb4bfc2', 'Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0); // -0
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+
+        curl_exec($ch);
+        curl_close($ch);
+
+        error_log("Update Event M2X Succesful " . $ch , 3, "../storage/logs/myerror.log");
     }
 
     public function sendLocationM2X($location){
-        $url = "http://api-m2x.att.com/v2/devices/8394935169b8cea3b9495998de0f8071/location";
+
+        $url = "http://api-m2x.att.com/v2/devices/" .  SELF::DEVICE_ID  . "/location";
         $data = array('name' => "Location", 'latitude' => $location['latitude'] ,'longitude' => $location['longitude'], 'elevation' => $location['elevation']);
         $json = json_encode($data);
 
-        $options = array(
-            'http' => array(
-                'header'  => "X-M2X-KEY: ace59ecc3eec57c843e0afcc1eb4bfc2,
-                              Content-type: application/json",
-                'method'  => 'PUT',
-                'content' => $json
-            )
-        );
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); // -X
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-M2X-KEY: ace59ecc3eec57c843e0afcc1eb4bfc2', 'Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0); // -0
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        error_log("Update Event M2X Succesful " . $result , 3, "../storage/logs/myerror.log");
+        curl_exec($ch);
+        curl_close($ch);
+
+        error_log("Update Location M2X Succesful " . $ch , 3, "../storage/logs/myerror.log");
     }
 
 
